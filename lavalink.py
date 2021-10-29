f930f788064ebda5ee5016aa553781df56f750a7
あきしょう <akishoudayo@gmail.com>
2021-10-29 13:40:58 +0900

    Update lavalink.py

--- lavalink.py
+++ lavalink.py
@@ -1,6 +1,7 @@
-from os import getenv, system
-import urllib.request
-import re
+from os import getenv
+from subprocess import run
+from urllib.request import urlretrieve
+from re import sub
 
 
 print("[INFO] Changing the port ...")
@@ -15,13 +16,13 @@ else:
 
 print("[INFO] Downloading Latest Lavalink ...")
 try:
-    urllib.request.urlretrieve("https://github.com/freyacodes/Lavalink/releases/download/3.4/Lavalink.jar", filename='Lavalink.jar')
+    urlretrieve("https://github.com/freyacodes/Lavalink/releases/download/3.4/Lavalink.jar", filename='Lavalink.jar')
 except BaseException as exc:
     print(f"[ERROR] Error downloading Lavalink... Info: {exc}")
 else:
     print("[INFO] Success in downloading Lavalink...")
     
 try:
-    system('java -jar Lavalink.jar')
+    run('java -jar Lavalink.jar', shell=True)
 except:
     print(f"[ERROR] Failed to start Lavalink server... Info: {exc}")
